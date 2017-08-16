eb0635174d182385058688582c66e7a8c1d690de
Rule1001 <hello@rule1001.com>
2017-08-16 13:44:05 +0100

    added add property link

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import NewProperty from './NewProperty'
 
 class Navbar extends React.Component {
   render () {
     return (
         <nav>
           <h1>Java Properties</h1>
-          <Link className="topicLink" to="/">Home</Link>
+          <Link className="HomeLink" to="/">Home</Link>
+          <Link className="newPropertyLink" to="/newProperty">Add Property</Link>
         </nav>
     );
   }
