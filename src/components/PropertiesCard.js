592fae87559b6ee3e4d25d3ee66ecb05e83e3f43
Rule1001 <hello@rule1001.com>
2017-08-16 14:26:07 +0100

    formatting changes

--- src/components/PropertiesCard.js
+++ src/components/PropertiesCard.js
@@ -6,12 +6,12 @@ const PropertiesCard = function (props) {
   return (
     <div className="columns">
       <div className="column is-half">
-        <p className="notification is-info">
+        <ul className="notification is-info">
           <p className=''>{property.propertyId}</p>
           <p className=''>{property.propertyType}</p>
           <p className=''>{property.numBedrooms}</p>
           <p className=''>{property.location}</p>
-        </p>
+        </ul>
       </div>
     </div>
   );
