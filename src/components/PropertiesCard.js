2eaa3a219efed0c4c5278e9aea362d731cd658bb
Rule1001 <hello@rule1001.com>
2017-08-16 12:24:08 +0100

    propertiesCard displaying all Properties

---
+++ src/components/PropertiesCard.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const PropertiesCard = function (props) {
+  console.log(props.property, 'props')
+  const property = props.property
+  return (
+    <div className="columns">
+      <div className="column is-half">
+        <p className="notification is-info">
+          <p className=''>{property.propertyId}</p>
+          <p className=''>{property.propertyType}</p>
+          <p className=''>{property.numBedrooms}</p>
+          <p className=''>{property.location}</p>
+        </p>
+      </div>
+    </div>
+  );
+};
+
+
+export default PropertiesCard;
