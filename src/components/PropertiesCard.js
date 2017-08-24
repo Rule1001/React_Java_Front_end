9048eb9debb5552c3bcac652b99576bb22d15568
Rule1001 <hello@rule1001.com>
2017-08-24 16:41:08 +0100

    Properties now fully displaying

--- src/components/PropertiesCard.js
+++ src/components/PropertiesCard.js
@@ -1,19 +1,31 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom';
 
 const PropertiesCard = function (props) {
-  const property = props.property
   return (
-    <div className="columns">
-      <div className="column is-half">
-        <ul className="notification is-info">
-          <p className=''>{property.propertyId}</p>
-          <p className=''>{property.propertyType}</p>
-          <p className=''>{property.numBedrooms}</p>
-          <p className=''>{property.location}</p>
-        </ul>
-      </div>
+    <div className='box'>
+      <article className="media">
+        <div className="media-content">
+          <div className="content">
+            <Link to={'/properties/' + props.property.propertyId}>
+              <h2 className="title is-3">{props.property.location}</h2>
+              <div className="columns">
+                <div className="column is-half">
+                  <ul className="notification is-info">
+                    <p className=''>{props.property.propertyId}</p>
+                    <p className=''>{props.property.propertyType}</p>
+                    <p className=''>{props.property.numBedrooms}</p>
+                    <p className=''>{props.property.location}</p>
+                  </ul>
+                </div>
+              </div>
+            </Link>
+          </div>
+        </div>
+      </article>
     </div>
+
   );
 };
 
