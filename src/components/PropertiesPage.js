d5686dc3927ace6f5e89d06180e85b95f3178337
Rule1001 <hello@rule1001.com>
2017-08-24 16:41:28 +0100

    added properties page. Linked and displaying

--- src/components/PropertiesPage.js
+++ src/components/PropertiesPage.js
@@ -1,60 +1,54 @@
-// import React from 'react';
-// import PropTypes from 'prop-types';
-// // import ArticleTitleBar from './ArticleTitleBar';
-// import PropertyText from './PropertyText';
-// // import VoteButton from './VoteButton';
-// // import NewComment from './NewComment';
-// // import CommentList from './CommentList';
-// // import Profile from './Profile';
-// import {connect} from 'react-redux';
-// import * as actions from '../actions/actions';
-// import '../../public/style.css';
+import React from 'react';
+import PropTypes from 'prop-types';
+import PropertyText from './PropertyText';
+import { connect } from 'react-redux';
+import * as actions from '../actions/actions';
+import '../../public/style.css';
 
-// class PropertiesPage extends React.Component {
-//   componentDidMount () {
-//     this.props.fetchPropertiesByID(this.props.match.params.propertyId);
-//     this.props.fetchPropertiesByPropertyID(this.props.match.params.propertyId);
-//   console.log(this.props)
-//   }
+class PropertiesPage extends React.Component {
+    componentDidMount() {
+        this.props.fetchPropertiesByID(this.props.match.params.propertyId);
+    }
 
-//   render () {
-//     return (
-//       <div className="propertiesPage columns">
-//         <div className="column is-three-quarters">
-//           <ArticleText 
-//             body={this.props.selectedProperty.body} 
-//           />
-//           <div className="voteAndComment">
-//           </div>
+    render() {
+        return (
+            <div className="propertiesPage columns">
+                <div className="column is-three-quarters">
+                Your chosen display property is
+                    <div className="columns">
+                        <div className="column is-half">
+                            <ul className="notification is-info">
+                                <p className='Location '>{this.props.selectedProperty.location}</p>
+                                <p className='Property Type '>{this.props.selectedProperty.propertyType}</p>
+                                <p className='Number of Bedrooms'>{this.props.selectedProperty.numBedrooms}</p>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        );
+    }
+}
 
-             
-//         </div>
-//       </div>
-//     );
-//   }
-// }
+function mapDispatchToProps(dispatch) {
+    return {
+        fetchPropertiesByID: (id) => {
+            dispatch(actions.fetchPropertiesByID(id));
+        }
+    };
+}
 
-// function mapDispatchToProps (dispatch) {
-//   return {
-//     fetchPropertiesByID: (id) => {
-//       dispatch(actions.fetchPropertiesByID(id));
-//     }
-//   };
-// }
+function MapStateToProps(state) {
+    return {
+        selectedProperty: state.selectedProperty,
+        loading: state.loading
+    };
+}
 
-// function MapStateToProps (state) {
-//   return {
-//     selectedProperty: state.selectedProperty,
-//     loading: state.loading
-//   };
-// }
+PropertiesPage.propTypes = {
+    selectedProperty: PropTypes.object.isRequired,
+    loading: PropTypes.bool.isRequired,
+    fetchPropertiesByID: PropTypes.func.isRequired,
+};
 
-// PropertiesPage.propTypes = {
-//     selectedProperties: PropTypes.object.isRequired,
-//     loading: PropTypes.bool.isRequired,
-//     fetchPropertiesByID: PropTypes.func.isRequired,
-//     fetchPropertiesByPropertiesID: PropTypes.func.isRequired,
-//     match: PropTypes.object.isRequired
-// };
-
-// export default connect(MapStateToProps, mapDispatchToProps) (PropertiesPage);
+export default connect(MapStateToProps, mapDispatchToProps)(PropertiesPage);
